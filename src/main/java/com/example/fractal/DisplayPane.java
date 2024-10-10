7ed643a2680c5e8a53c8de29b7899193945d8569
Natt Decodes <nattcraft16@gmail.com>
2024-10-10 13:17:33 -0400

    Update DisplayPane.java

--- src/main/java/com/example/fractal/DisplayPane.java
+++ src/main/java/com/example/fractal/DisplayPane.java
@@ -1,5 +1,6 @@
 package com.example.fractal;
 
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 
@@ -7,7 +8,14 @@ import java.util.Objects;
 
 public class DisplayPane {
     public VBox displayThePain() {
+        VBox displayPane = new VBox();
         Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/fractal.png")));
-        
+
+        Button button = new Button("download");
+        button.setLayoutX(100);
+        button.setLayoutY(300);
+        displayPane.getChildren().add(button);
+
+        return displayPane;
     }
 }

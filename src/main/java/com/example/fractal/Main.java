8fbc8300bd3896cbb0bdb1dacaf1541c6386b957
yara <ghamlouchyara68@gmail.com>
2024-10-17 13:07:55 -0400

    Put the two panes together

--- src/main/java/com/example/fractal/Main.java
+++ src/main/java/com/example/fractal/Main.java
@@ -2,6 +2,7 @@ package com.example.fractal;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,10 +14,10 @@ public class Main extends Application {
         javafx.scene.control.MenuBar menuBar = new MenuBar().make();
 
         vbox1.getChildren().addAll(menuBar);
-        //DisplayPane displayPane = new DisplayPane();
-        //vbox1.getChildren().add(displayPane.displayThePain());
+        DisplayPane displayPane = new DisplayPane();
         ParameterPane parameterPane = new ParameterPane();
-        vbox1.getChildren().add(parameterPane.displayParamPane());
+        HBox pains = new HBox(parameterPane.displayParamPane(), displayPane.displayThePain());
+        vbox1.getChildren().addAll(pains);
         primaryStage.setScene(scene);
         primaryStage.setTitle("aHHHHHHHHHH GET ME OUT OF HERE");
         primaryStage.show();
